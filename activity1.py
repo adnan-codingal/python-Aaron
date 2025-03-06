36bf1652efadd2f82d30f379988afa4b6a3c983e
adnan-codingal <adnan.codingal@gmail.com>
2025-03-06 22:18:04 +0530

    Delete activity1.py

--- activity1.py
+++
@@ -1,8 +0,0 @@
-# i am printing the hello world, - This is my first activity
-print('Hello world')
-# YOUR NAMEsni
-print("I am Adnan")
-# YOUR GRADE
-# YOUR FAVOURITE COLOR
-# YOUR FAVOURITE HOBBY
-# YOUR FAVURITE PLANE MODEL
