07cb1c5994b1016a4bb591c9794bd75b790755e7
adnan-codingal <adnan.codingal@gmail.com>
2025-03-06 22:11:16 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,8 @@
+# i am printing the hello world, - This is my first activity
+print('Hello world')
+# YOUR NAMEsni
+print("I am Adnan")
+# YOUR GRADE
+# YOUR FAVOURITE COLOR
+# YOUR FAVOURITE HOBBY
+# YOUR FAVURITE PLANE MODEL
